b4893c07a5ca2731b5d7303f6a8c5b95fc31601a
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-12-26 12:01:17 +0200

    Move utility functions into main source file
    All utility functions from utils.cpp were moved into Finance-Tracker.cpp. The utils.cpp file was deleted and project files were updated.

--- Finance-Tracker/Finance-Tracker/Finance-Tracker.cpp
+++ Finance-Tracker/Finance-Tracker/Finance-Tracker.cpp
@@ -283,6 +283,16 @@ void report()
     printReportSummary(totalIncome, totalExpense);
 }
 
+int myStringLength(const char* text)
+{
+    int lengh = 0;
+    while (text[lengh] != TERMINATE_SYMBOL)
+    {
+        lengh++;
+    }
+    return lengh;
+}
+
 void printReportHeader()
 {
     std::cout << "Month | Income | Expense | Balance";
@@ -594,4 +604,104 @@ void printChartMonths()
             << " ";
     }
     newLine();
+}
+int myStringCompare(const char* firstString, const char* secondString)
+{
+    int i = 0;
+    while (firstString[i] != TERMINATE_SYMBOL && secondString[i] != TERMINATE_SYMBOL)
+    {
+        if (firstString[i] < secondString[i])
+        {
+            return -1;
+        }
+        if (firstString[i] > secondString[i])
+        {
+            return 1;
+        }
+        i++;
+    }
+    if (firstString[i] == TERMINATE_SYMBOL && secondString[i] == TERMINATE_SYMBOL)
+    {
+        return 0;
+    }
+    if (firstString[i] == TERMINATE_SYMBOL)
+    {
+        return -1;
+    }
+    return 1;
+}
+
+double myAbs(double value)
+{
+    if (value < 0)
+    {
+        return -value;
+    }
+    return value;
+}
+
+void newLine()
+{
+    std::cout << std::endl;
+}
+
+void myStringConcat(char* sourceString, char* destinationString)
+{
+    while (*destinationString != '\0')
+    {
+        destinationString++;
+    }
+    while (*sourceString != '\0')
+    {
+        *destinationString = *sourceString;
+        sourceString++;
+        destinationString++;
+    }
+    *destinationString = '\0';
+}
+
+char* getArgumentFromCommand(const char* fullCommand)
+{
+    char* argumentString = new char[30];
+    argumentString[0] = TERMINATE_SYMBOL;
+    int i = 0;
+    while (fullCommand[i] != TERMINATE_SYMBOL && fullCommand[i] != ' ')
+    {
+        i++;
+    }
+    while (fullCommand[i] == ' ')
+    {
+        i++;
+    }
+    myStringConcat((char*)(fullCommand + i), argumentString);
+    return argumentString;
+}
+
+char* getCommandWord(const char* fullCommand)
+{
+    char* commandWord = new char[30];
+    int i = 0;
+    while (fullCommand[i] != TERMINATE_SYMBOL && fullCommand[i] != ' ')
+    {
+        commandWord[i] = fullCommand[i];
+        i++;
+    }
+    commandWord[i] = TERMINATE_SYMBOL;
+    return commandWord;
+}
+
+void printBalanceColored(double balance)
+{
+    if (balance > 0)
+    {
+        std::cout << "\033[1;32m+" << balance << "\033[0m";
+    }
+    else if (balance < 0)
+    {
+        std::cout << "\033[1;31m" << balance << "\033[0m";
+    }
+    else
+    {
+        std::cout << "\033[1;33m" << balance << "\033[0m";
+    }
 }
