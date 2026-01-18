e15b0666e9cd43d4eb45015074b4661ae6f87a3c
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2026-01-18 10:46:57 +0200

    Add project and author info to header files
    Added standardized file headers to all .h files, including project details, author name, FN number, compiler, and a brief description of each file's purpose.

--- Finance-Tracker/Finance-Tracker/forecast.h
+++ Finance-Tracker/Finance-Tracker/forecast.h
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project #3
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Katerina Dimitrova
+* @idnumber 8MI0600563
+* @compiler GCC
+*
+* Header file with forecast command declarations
+*
+*/
+
 #pragma once
 #include "structures.h"
 
